955ea1bca11abb943ab741fe7a4bc12686ebe8df
LaurenBanoud4 <laurentema286@gmail.com>
2026-03-23 19:36:01 +0200

    Add DoctorSpecialty entity for doctor-specialty mapping
    Introduced DoctorSpecialty class in MediConnect.Models to represent the relationship between doctors and specialties, including navigation properties and data annotations for EF mapping. Note: The class is currently nested, which may need revision.

---
+++ MediConnect/Models/DoctorSpecialty.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MediConnect.Models
+{
+    internal class DoctorSpecialty
+    {
+        public class DoctorSpecialty
+        {
+            [Key]
+            public int Id { get; set; }
+
+            public int DoctorId { get; set; }
+            public int SpecialtyId { get; set; }
+
+            public bool IsPrimary { get; set; } = false;
+
+            // Navigation
+            [ForeignKey(nameof(DoctorId))]
+            public DoctorProfile Doctor { get; set; } = null!;
+
+            [ForeignKey(nameof(SpecialtyId))]
+            public Specialty Specialty { get; set; } = null!;
+        }
+    }
+}
