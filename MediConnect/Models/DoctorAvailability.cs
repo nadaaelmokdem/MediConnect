28472b410c627b6202e03bdbbc0a487b28116850
LaurenBanoud4 <laurentema286@gmail.com>
2026-03-23 19:38:50 +0200

    Add DoctorAvailability entity to manage doctor schedules
    Introduced the DoctorAvailability class in the MediConnect.Models namespace. This entity includes properties for doctor ID, day of week, time slots, slot duration, active status, and a navigation property to DoctorProfile, using data annotations for EF mapping.

---
+++ MediConnect/Models/DoctorAvailability.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MediConnect.Models
+{
+    internal class DoctorAvailability
+    {
+        public class DoctorAvailability
+        {
+            [Key]
+            public int AvailabilityId { get; set; }
+
+            public int DoctorId { get; set; }
+
+            public DayOfWeek DayOfWeek { get; set; }
+
+            public TimeSpan StartTime { get; set; }
+            public TimeSpan EndTime { get; set; }
+
+            public int SlotDurationMins { get; set; } = 30;
+
+            public bool IsActive { get; set; } = true;
+
+            // Navigation
+            [ForeignKey(nameof(DoctorId))]
+            public DoctorProfile Doctor { get; set; } = null!;
+        }
+    }
+}
